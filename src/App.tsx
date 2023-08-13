e38a4146f59cebb564fb12f4dbf08d0cac6e08b2
unional <homawong@gmail.com>
2023-08-13 12:09:43 -0700

    clarify case

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { userEvent } from '@storybook/testing-library'
 
 function App() {
   const [inputText, setInputText] = useState('')
 
+
+  const onChange = useCallback((e) => setInputText(e.target.value), [])
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,9 +17,9 @@ function App() {
   return (
     <>
       <div>
-        Make some change to the <code>App.tsx</code> and save the file to cause HMR.
+        Make some change to the <code>App.tsx</code> that adds a node in the DOM, e.g. add some string above this div.
       </div>
-      <input id="input" onChange={(e) => setInputText(e.target.value)} />
+      <input id="input" onChange={onChange} />
       <div>
         state value: {inputText}
       </div>
